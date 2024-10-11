da6b3658f6fef9c013afe067df45f9e62847e571
Nunzia Biele <nunzia.biele@virgilio.it>
2024-10-11 11:37:49 +0200

    creazione backend

--- server.js
+++ server.js
@@ -1,17 +1,50 @@
-const express = require("express");
-const app = express();
+import express from "express";
+import "express-async-error";
+import morgan from "morgan";
+import cors from "cors";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import mysql from 'mysql'
 
+const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Welcome to my Server!"));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
 
-app.get('/1', (req, res) => {
-    res.send('Questa è la pagina numero 1');
+app.get("/info", (req, res) => {
+  res.status(200).json({ msg: "success!" });
+});
+
+app.post("/login", async (req, res) => {
+  const token = "keycode";
+  const { email, password } = req.body;
+  const criptedPassword = await bcrypt.hash(password, 10);
+  
+  res.status(200).json({
+    msg: "success",
+    apiToken: token,
+    email: email,
+    password: criptedPassword,
   });
+});
 
-  app.get('/2', (req, res) => {
-    res.send('Questa è la pagina numero 2');
+app.post("/registrazione", async (req, res) => {
+  const token = "keycode";
+  const { username, email, phone, password } = req.body;
+  const criptedPassword = await bcrypt.hash(password, 10);
+
+  res.status(200).json({
+    msg: "success!",
+    apiToken: token,
+    username: username,
+    email: email,
+    phone: phone,
+    password: criptedPassword,
   });
+});
 
-
-app.listen(port, () => console.log(`Server in ascolto su http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server in ascolto su http://localhost:${port}`)
+);
